2018aaa50259f686c610a1d454c8d03792d13396
hogehoge821 <mmath7786@gmail.com>
2023-08-19 12:57:55 +0900

    add addition and subtraction

--- 9cc.c
+++ 9cc.c
@@ -7,9 +7,26 @@ int main(int argc, char **argv) {
         fprintf(stderr, "incorrect number of arguments");
         return 1;
     }
+    char *p = argv[1];
+
     puts(".globl main");
     puts("main:");
-    printf("  mov x0, %d\n", atoi(argv[1]));
+    printf("  mov w0, %ld\n", strtol(p, &p, 10));
+
+    while (*p) {
+        if (*p == '+') {
+            p++;
+            printf("  add w0, w0, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        if (*p == '-') {
+            p++;
+            printf("  sub w0, w0, %ld\n", strtol(p, &p, 10));
+            continue;
+        }
+        fprintf(stderr, "unexpected char: '%c'\n", *p);
+        return 1;
+    }
     puts("  ret");
     return 0;
 }
